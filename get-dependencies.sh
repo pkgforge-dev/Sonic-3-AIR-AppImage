df4d18dd61724b9cb2eebae7e74ebb22660a549c
Link4Electronics <link4electronics@gmail.com>
2026-02-16 21:57:01 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -58,14 +58,11 @@ else
     #sed -i '113,125s|^|//|w /dev/stdout' Oxygen/sonic3air/source/sonic3air/client/crowdcontrol/CrowdControlClient.cpp
     #cd ..
     cd ./sonic3air
-    #patch -p1 < ../0001-fix-sdl-pipewire.patch
-    #ar rcs libdiscord_game_sdk.a
-    #cp libdiscord_game_sdk.a Oxygen/sonic3air/source/external/discord_game_sdk/lib/x86_64/
     sed -i 's/pw_node_enum_params(node->proxy/pw_node_enum_params((struct pw_node*)node->proxy/g' framework/external/sdl/SDL2/src/audio/pipewire/SDL_pipewire.c
     cd Oxygen/sonic3air/build/_cmake
     #cd ./sonic3air/Oxygen/sonic3air/build/_cmake
     #sed -i 's/set(CMAKE_CXX_FLAGS_RELEASE "-O3")/set(CMAKE_CXX_FLAGS_RELEASE "-O0")/' CMakeLists.txt
-    # -DUSE_DISCORD=OFF for aarch64
+    # -DUSE_DISCORD=false for aarch64
     sed -i -e 's|link_directories("${WORKSPACE_DIR}/Oxygen/|#&|' \
        -e 's|target_link_libraries(discord_game_sdk_source|#&|' \
        -e 's|set(CMAKE_EXE_LINKER_FLAGS.*-rpath|#&|' \
